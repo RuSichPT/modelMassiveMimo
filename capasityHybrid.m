98bdc3e90a0ed650ff235c44ea1efece0afb83ba
pavel.tokarev <pavel.tokarev@mail.ntc51>
2022-10-19 17:29:58 +0300

    add compare capacity NeuralNetwork

--- capasityHybrid.m
+++ capasityHybrid.m
@@ -4,13 +4,20 @@ set(0,'DefaultAxesFontName','Times New Roman');
 set(0,'DefaultTextFontName','Times New Roman'); 
 flag_chanel = 'STATIC';
 SNR = 0:31; % диапазон SNR
-prm.numTx = 8; % Кол-во излучающих антен 
+prm.numTx = 64; % Кол-во излучающих антен 
 prm.numRx = 4; % Кол-во приемных антен
 prm.numUsers = prm.numRx;
 prm.numSTS = prm.numRx;
 prm.numCarriers = 1;
 prm.numSTSVec = ones(1,prm.numUsers);
-[H_chan,H_siso,~] = create_chanel(flag_chanel,prm);
+load('DataBase/NeuralNetwork/H_I.txt')
+load('DataBase/NeuralNetwork/H_Q.txt')
+load('DataBase/NeuralNetwork/F_rf_I.txt')
+load('DataBase/NeuralNetwork/F_rf_Q.txt')
+load('DataBase/NeuralNetwork/C_DRL.txt')
+Frf_NN = F_rf_I+1i*F_rf_Q;
+H_chan = H_I+1i*H_Q;
+H_chan = H_chan';
 H = permute(H_chan,[3,1,2]);
 Hcell = cell(prm.numUsers,1);
 for i = 1:prm.numUsers
@@ -21,33 +28,29 @@ end
 Frf = Frf';
 H_chan = H_chan';
 Heff = H_chan*Frf;
+Heff_NN = H_chan*Frf_NN;
 indExp = 1;
 C_hybrid_mimo = zeros(length(indExp),length(SNR));
+C_hybrid_NN = zeros(length(indExp),length(SNR));
 C_mimo = zeros(length(indExp),length(SNR));
-C_siso = zeros(length(indExp),length(SNR));
 for indSNR = 1:length(SNR)    
-%     C_mimo_CU(indExp,indSNR) = mimo_capacity_CU(H_chan, indSNR, prm.numTx);        
-%     C_mimo_CK(indExp,indSNR) = mimo_capacity_CK(H_chan, indSNR, prm.numTx);
-    C_hybrid_mimo(indExp,indSNR) = mimoCapacity(Heff, indSNR, prm.numSTS);
-    C_mimo(indExp,indSNR) = mimoCapacity(H_chan, indSNR, prm.numSTS);
-    C_siso(indExp,indSNR) = siso_capacity(H_siso,indSNR);
+%     C_hybrid_mimo(indExp,indSNR) = mimo_capacity_CU(Heff, SNR(indSNR), prm.numTx);
+%     C_hybrid_NN(indExp,indSNR) = mimo_capacity_CU(Heff_NN, SNR(indSNR), prm.numTx);
+%     C_mimo(indExp,indSNR) = mimo_capacity_CK(H_chan, SNR(indSNR), prm.numTx);
+    C_hybrid_mimo(indExp,indSNR) = mimoCapacity(Heff, SNR(indSNR), prm.numSTS);
+    C_hybrid_NN(indExp,indSNR) = mimoCapacity(Heff_NN, SNR(indSNR), prm.numSTS);
+    C_mimo(indExp,indSNR) = mimoCapacity(H_chan, SNR(indSNR), prm.numSTS);
 end
 figure();
-plot(SNR,mean(C_hybrid_mimo,1),'-*k','LineWidth',1.5);
-% plot(SNR,mean(C_mimo_CU,1),'k','LineWidth',1.5);
+plot(SNR,C_hybrid_mimo,'-*k','LineWidth',1.5);
 hold on;
-plot(SNR,mean(C_mimo,1),'--k','LineWidth',1.5);
-% plot(SNR,mean(C_mimo_CK,1),'--k','LineWidth',1.5);
-plot(SNR,mean(C_siso,1),'-.k','LineWidth',1.5);
+plot(SNR,C_hybrid_NN,'k','LineWidth',1.5);
+plot(SNR,C_mimo,'--k','LineWidth',1.5);
 grid on;
-xlim([0 SNR(end)]);
+xlim([SNR(1) SNR(end)]);
 xlabel('Отношение сигнал/шум, дБ');
 ylabel('C, бит/с/Гц');
 str1 = ['hybrid mMIMO JSDM ' num2str(prm.numTx) 'x' num2str(prm.numRx)];
-str2 = ['optimal mMIMO ' num2str(prm.numTx) 'x' num2str(prm.numRx)];
-% str2 = ['mMIMO ' num2str(prm.numTx) 'x' num2str(prm.numRx) ' CU'];
-% str3 = ['mMIMO ' num2str(prm.numTx) 'x' num2str(prm.numRx) ' CK'];
-legend(str1,str2,'SISO','location','northwest');
-
-% C_hybrid_mimo = C_hybrid_mimo';
-% save('DataBase/Capacity/data.txt','C_hybrid_mimo','-ascii');
+str2 = ['hybrid mMIMO NN ' num2str(prm.numTx) 'x' num2str(prm.numRx)];
+str3 = ['optimal mMIMO ' num2str(prm.numTx) 'x' num2str(prm.numRx)];
+legend(str1,str2,str3,'location','northwest');
