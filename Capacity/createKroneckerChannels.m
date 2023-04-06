95d846a09e775489a4aa1431add245d2e0ed0525
pavel.tokarev <pavel.tokarev@mail.ntc51>
2023-04-06 11:19:45 +0300

    Перенес в другие репозиторий https://github.com/RuSichPT/capacityOptimization https://github.com/RuSichPT/matlabFunctions

--- Capacity/createKroneckerChannels.m
+++
@@ -1,9 +0,0 @@
-function Hk = createKroneckerChannels(numTx,numRx,numChan,R,Z)
-    addpath("../functions");
-    Hk = zeros(numRx,numTx,numChan);
-    for k = 1:numChan
-        H = createStaticChannel(numTx,numRx);
-        H = H.';
-        Hk(:,:,k) = H*sqrt(R)*Z;
-    end
-end
--- functions/createStaticChannel.m
+++
@@ -1,8 +0,0 @@
-function H = createStaticChannel(numTx,numRx)
-    H = zeros(numTx,numRx);
-    for i = 1:numTx
-        for j = 1:numRx
-            H(i,j) = (randn(1)+1i*randn(1))/sqrt(2);
-        end
-    end
-end
